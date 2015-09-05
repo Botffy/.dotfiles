f6ec3e42363c40f6fc69cbc38839f67bb8193e24
Ármin Scipiades <scipiades.armin@gmail.com>
2015-09-05 11:39:24 +0200

    Add Sublime User directory

---
+++ sublime/export.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+rm -r $(cygpath $USERPROFILE)/AppData/Roaming/Sublime\ Text\ 3/Packages/User
+cp -r User $(cygpath $USERPROFILE)/AppData/Roaming/Sublime\ Text\ 3/Packages/
---
+++ sublime/import.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+rm -r User
+cp -r $(cygpath $USERPROFILE)/AppData/Roaming/Sublime\ Text\ 3/Packages/User .
