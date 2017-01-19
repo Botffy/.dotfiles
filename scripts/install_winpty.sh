bca03421ee6da2fa715355dfed4f9870e34cabeb
Ármin Scipiades <armin.scipiades@egroup.hu>
2017-01-19 18:57:08 +0100

    Add winpty install script <3

---
+++ scripts/install_winpty.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# from
+# https://github.com/grant-humphries/dotfiles/blob/master/scripts/install_winpty.sh
+
+# install winpty to clean install of babun/cygwin
+# winpty project: https://github.com/rprichard/winpty
+
+set -e
+
+winpty_github='https://github.com/rprichard/winpty.git'
+winpty_dir=$(basename "${winpty_github}" '.git' )
+install_dir="${HOME}/.local"
+
+dependencies() {
+    # install dependencies with babun's pact, note that gcc-g++ and
+    # gcc-core must be at the same version, this should take care of
+    # that
+    pact install mingw64-i686-gcc-g++
+    pact install gcc-g++
+    pact update gcc-core
+}
+
+clone() {
+    if [ ! -d "${TMP}/${winpty_dir}" ]; then
+        # clone the winpty repo
+        cd "${TMP}"
+        git clone "${winpty_github}"
+    fi
+}
+
+compile() {
+    cd "${TMP}/${winpty_dir}"
+    ./configure
+    make
+    make install PREFIX="${install_dir}"
+}
+
+dependencies
+clone
+compile
